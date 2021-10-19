755263d674cf567e9a0dcf59b3b758d64fc59928
manchala meherish <manchala.meherish1524@gmail.com>
2021-10-19 22:42:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def login():
     return render_template("login.html")
 
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 5000))
-   app.run(host='0.0.0.0', port=port, debug=True)
+   port = int(os.environ.get("PORT", 8000))
+   app.run(host='0.0.0.0.8000', port=port, debug=True)
