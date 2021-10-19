71610e191a1ebb1e0b746481a4005c03a7114a58
manchala meherish <manchala.meherish1524@gmail.com>
2021-10-19 22:21:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def login():
         mydb.close()
         return redirect(url_for('reg'))
     return render_template("login.html")
+
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, use_reloader=True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(debug=True, port=port)
